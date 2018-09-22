c5bf23c7c7c4ed31d57cb965a83daa4f75c5f20a
Thomas Feng <feng.thom@gmail.com>
2018-09-22 15:47:56 -0400

    added jsonSerialization to PhotoStore

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -20,8 +20,11 @@ class PhotoStore {
         let request = URLRequest(url: url)
         let task = session.dataTask(with: request) { (data, response, error) -> Void in //This closure is called when this request finishes and data is recieved if possible
             if let jsonData = data {
-                if let jsonString = String(data: jsonData, encoding: .utf8) {
-                    print(jsonString)
+                do {
+                    let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: [])
+                    print(jsonObject)
+                } catch let error {
+                    print("Error creating json Object: \(error)")
                 }
             } else if let requestError = error {
                 print("Error fetching interesting photos: \(requestError)")

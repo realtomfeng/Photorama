9c8000801d5fdcac19b40b972b3496fce83ab00f
Thomas Feng <feng.thom@gmail.com>
2018-09-22 17:09:49 -0400

    Parsed JSON Data to Photo Objects

--- Photorama/FlickrAPI.swift
+++ Photorama/FlickrAPI.swift
@@ -30,11 +30,8 @@ struct FlickrAPI {
     }()
     
     private static func flickrURL(method: Method, parameters: [String:String]?) -> URL {
-        
         var components = URLComponents(string: baseURLString)!
-        
         var queryItems = [URLQueryItem]()
-        
         let baseParams = [
             "method": method.rawValue,
             "format": "json",
@@ -58,7 +55,22 @@ struct FlickrAPI {
         return components.url!
     }
     
-    static func photos(fromJSON data: Data) -> PhotoResult {
+    private static func photo(fromJSON json: [String:Any]) -> Photo? {
+        guard
+            let photoID = json["id"] as? String,
+            let title = json["title"] as? String,
+            let dateString = json["datetaken"] as? String,
+            let photoURLString = json["url_h"] as? String,
+            let url = URL(string: photoURLString),
+            let dateTaken = dateFormatter.date(from: dateString)
+            else {
+                return nil
+        }
+        
+        return Photo(title: title, remoteURL: url, photoID: photoID, dateTaken: dateTaken)
+    }
+    
+    static func photos(fromJSON data: Data) -> PhotosResult {
         do {
             let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
             guard
@@ -69,6 +81,14 @@ struct FlickrAPI {
                         return .failure(flickrError.invalidJSONData)
             }
             var finalPhotos = [Photo]()
+            for photoJSON in photosArray {
+                if let photo = photo(fromJSON: photoJSON) {
+                finalPhotos.append(photo)
+                }
+            }
+            if finalPhotos.isEmpty && !photosArray.isEmpty {
+                return .failure(flickrError.invalidJSONData)
+            }
             return .success(finalPhotos)
         } catch let error {
             return .failure(error)
--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-enum PhotoResult {
+enum PhotosResult {
     case success([Photo])
     case failure(Error)
 }
@@ -20,23 +20,21 @@ class PhotoStore {
         return URLSession(configuration: config)
     }()
     
-    func fetchInterestingPhotos() {
+    private func processPhotosRequest(data: Data?, error: Error?) -> PhotosResult {
+            guard let jsonData = data else {
+                return .failure(error!)
+            }
+            return FlickrAPI.photos(fromJSON: jsonData)
+    }
+    
+    
+    func fetchInterestingPhotos(completion: @escaping (PhotosResult) -> Void) {
         let url = FlickrAPI.interestingPhotosURL
         let request = URLRequest(url: url)
         let task = session.dataTask(with: request) { (data, response, error) -> Void in //This closure is called when this request finishes and data is recieved if possible
-            if let jsonData = data {
-                do {
-                    let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: [])
-                    print(jsonObject)
-                } catch let error {
-                    print("Error creating json Object: \(error)")
-                }
-            } else if let requestError = error {
-                print("Error fetching interesting photos: \(requestError)")
-            } else {
-                print("Unexpected error with the request")
-            }
+            let result = self.processPhotosRequest(data: data, error: error)
+            completion(result)
         }
-        task.resume()
+    task.resume()
     }
 }

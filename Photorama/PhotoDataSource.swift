2e4967349189c240f0b14862d9b8a2ccce5666bc
Thomas Feng <feng.thom@gmail.com>
2018-09-30 21:30:27 -0400

    added class PhotoDataSource

---
+++ Photorama/PhotoDataSource.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoDataSource.swift
+//  Photorama
+//
+//  Created by Thomas Feng on 9/30/18.
+//  Copyright © 2018 Infinity and Beyond. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    var photos = [Photo]()
+}

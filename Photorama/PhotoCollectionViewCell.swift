05ffc635841449c4650ffc6d96d34c5effa67764
Thomas Feng <feng.thom@gmail.com>
2018-10-02 20:10:41 -0400

    Added PhotoCollectionViewCell and set up the UIImage and Spinner in storyboard

---
+++ Photorama/PhotoCollectionViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama
+//
+//  Created by Thomas Feng on 10/2/18.
+//  Copyright © 2018 Infinity and Beyond. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
+    func update(with image: UIImage?) {
+        if let imageToDisplay = image {
+            spinner.stopAnimating()
+            imageView.image = imageToDisplay
+        } else {
+            spinner.startAnimating()
+            imageView.image = nil
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        update(with: nil)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        update(with: nil)
+    }
+}
